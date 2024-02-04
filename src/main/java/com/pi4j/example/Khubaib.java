28acfc3f814728456b4efffc0cf6f0ebe97c7598
mail2niaz-LENOVO <mail2niazahamed@gmail.com>
2024-02-04 12:41:17 +0530

    khubaib working java

--- src/main/java/com/pi4j/example/Khubaib.java
+++ src/main/java/com/pi4j/example/Khubaib.java
@@ -5,23 +5,26 @@ import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.gpio.digital.DigitalState;
 
-public class Musab {
+import java.security.Timestamp;
+import java.time.LocalDateTime;
+
+public class Khubaib {
 
     Context pi4j = Pi4J.newAutoContext();
 
     private final int[] GPIO_PIN = {5, 6, 13, 19};
     private DigitalOutput[] motor_pins = new DigitalOutput[4];
-    private final int STEP_SLEEP = 2;
-    private final int STEP_COUNT = 4096;
+    private final int STEP_SLEEP = 10;
+    private final int STEP_COUNT = 2098;
     private final boolean DIRECTION = true;
-    private final int[][] STEP_SEQUENCE = {{1, 0, 0, 1},
-            {1, 0, 0, 0},
-            {1, 1, 0, 0},
-            {0, 1, 0, 0},
+    private final int[][] STEP_SEQUENCE = {{1, 1, 0, 0},
             {0, 1, 1, 0},
-            {0, 0, 1, 0},
             {0, 0, 1, 1},
-            {0, 0, 0, 1}};
+            {1, 0, 0, 1},
+            {1, 1, 0, 0},
+            {0, 1, 1, 0},
+            {0, 0, 1, 1},
+            {1, 0, 0, 1}};
 
     {
         for (int i = 0; i < GPIO_PIN.length; i++) {
@@ -40,40 +43,40 @@ public class Musab {
     }
 
     public static void main(String[] args) throws Exception {
-        Musab musab = new Musab();
+        Khubaib musab = new Khubaib();
         musab.start();
     }
 
     public void start() throws Exception {
         try {
             for (int i = 0; i < STEP_COUNT; i++) {
-                if (motor_step_counter > 7) {
+                if (motor_step_counter > (STEP_SEQUENCE.length-1)) {
                     motor_step_counter = 0;
                 }
                 for (int pin = 0; pin < motor_pins.length; pin++) {
-                    System.out.println("i=" + i + ",pin=" + pin + ", state=" + STEP_SEQUENCE[motor_step_counter][pin] + ",motor_step_counter=" + motor_step_counter);
+                    System.out.println(LocalDateTime.now() +",i=" + i + ",pin=" + pin + ", state=" + STEP_SEQUENCE[motor_step_counter][pin] + ",motor_step_counter=" + motor_step_counter);
                     motor_pins[pin].setState(STEP_SEQUENCE[motor_step_counter][pin]);
                 }
                 if (DIRECTION) {
-                    System.out.println("b4 motor_step_counter" + motor_step_counter);
+                    System.out.println(LocalDateTime.now() +",b4 motor_step_counter" + motor_step_counter);
                     motor_step_counter++;
-                    System.out.println("after motor_step_counter" + motor_step_counter);
+                    System.out.println(LocalDateTime.now() +",after motor_step_counter" + motor_step_counter);
                 } else {
-                    System.out.println("b4 motor_step_counter" + motor_step_counter);
+                    System.out.println(LocalDateTime.now() +",b4 motor_step_counter" + motor_step_counter);
                     motor_step_counter++;
-                    System.out.println("after motor_step_counter" + motor_step_counter);
+                    System.out.println(LocalDateTime.now() +",after motor_step_counter" + motor_step_counter);
                 }
                 Thread.sleep(STEP_SLEEP);
 //                if(i == 3)
 //                    break;
             }
-            System.out.println("Stepper motor done.");
+            System.out.println(LocalDateTime.now() +",Stepper motor done.");
 
         } catch (Exception e) {
-            System.out.println("Uh oh, an error occurred: " + e.getMessage());
+            System.out.println(LocalDateTime.now() +",Uh oh, an error occurred: " + e.getMessage());
         } finally {
             cleanUp();
-            System.out.println("Stepper motor stopped.");
+            System.out.println(LocalDateTime.now() +",Stepper motor stopped.");
         }
     }
 
